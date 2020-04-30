4a88dc2d371f61e7146ba94e828a0f71a59d6c87
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-04-30 18:54:10 +0300

    Updated

---
+++ acmp/171/171/171.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long int x;
+
+	int s = 0;
+	int j = 1;
+	cin >> x;
+
+	for (long long int i = 2; i <= x; i++) {
+		while (x % i == 0) {
+			x /= i;
+			s++;
+		}
+		s++;
+		j *= s;
+		s = 0;
+	}
+
+	cout << j;
+
+	return 0;
+}
---
+++ acmp/64/64/64.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int SIZE;
+
+	int M;
+
+	cin >> M;
+
+	int* arr = new int[M];
+
+	for (int i = 0; i < M; i++) {
+		int n;
+		cin >> n;
+		arr[i] = n;
+	}
+
+	int maxNum = 0;
+	for (int i = 0; i < M; i++) {
+		if (arr[i] > maxNum) {
+			maxNum = arr[i];
+		}
+	}
+
+	/*
+	  maxNum - простое число до
+	  которого нужно искать простые числа
+	*/
+
+	int* sieve = new int[maxNum*2 + 1];
+	for (int i = 2; i <= maxNum*2; i++) {
+		sieve[i] = i;
+	}
+	int k = 2;
+	while(k*k < maxNum* 2) {
+		for (int i = 2; i <= maxNum* 2; i++) {
+			if (sieve[i] % k == 0 && sieve[i] >= 2 * k) {
+				sieve[i] = 0;
+			}
+		}
+		for (int i = 2; i <= maxNum* 2; i++) {
+			if (sieve[i] > k) {
+				k = sieve[i];
+				break;
+			}
+		}
+	}
+
+	std::string sieveStr;
+	for (int i = 2; i <= maxNum* 2; i++) {
+		if (sieve[i] != 0) {
+			sieveStr.append(to_string(sieve[i]));
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		cout << sieveStr[arr[i] - 1];
+	}
+
+	return 0;
+}
