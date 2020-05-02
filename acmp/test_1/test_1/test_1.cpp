09db85217a044425e2cb982f0721a957e5af49d6
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-05-02 14:19:13 +0300

    Updated

---
+++ acmp/test_1/test_1/test_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int P, K, S, sum;
+
+	cin >> sum;
+
+	for (int i = 1; i < sum; i++) {
+		P = S = i;
+		K = sum - P - S;
+
+		if (2 * (P + S) == K) {
+			cout << P << " " << K << " " << S;
+			break;
+		}
+	}
+
+	return 0;
+}
