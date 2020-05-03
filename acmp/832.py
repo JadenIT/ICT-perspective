e37de36e804493272366b228dbb1fc507503636b
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-05-03 22:47:55 +0300

    Updated

---
+++ acmp/832.py
@@ -0,0 +1,75 @@
+def f1(list):
+    newList = list.copy()
+    newList[0] -= 1
+    newList[1] -= 1
+    newList[2] += 1
+    return newList
+
+
+def f2(list):
+    newList = list.copy()
+    newList[0] -= 1
+    newList[1] += 1
+    newList[2] -= 1
+    return newList
+
+
+def f3(list):
+    newList = list.copy()
+    newList[0] += 1
+    newList[1] -= 1
+    newList[2] -= 1
+    return newList
+
+
+def function(list, size):
+
+    copied_list = list.copy()
+
+    for i in range(size):
+        s = 0
+        summary = 0
+        for j in range(3):
+            if copied_list[i][j] == 0:
+                s += 1
+            summary += copied_list[i][j]
+
+            if(s >= 2):
+                if(summary == 1):
+                    print('Yes')
+                    return
+                else:
+                    print('No')
+                    return
+
+    lastList = []
+
+    for i in range(size):
+        copied_list = list[i].copy()
+        if copied_list[0] != 0 and copied_list[1] != 0:
+            lastList.append(f1(copied_list))
+        if copied_list[0] != 0 and copied_list[2] != 0:
+            lastList.append(f2(copied_list))
+        if copied_list[1] != 0 and copied_list[2] != 0:
+            lastList.append(f3(copied_list))
+
+    function(lastList, len(lastList))
+
+
+N = int(input())
+
+arr = []
+
+for j in range(N):
+    tmp = []
+    for i in range(3):
+        t = int(input())
+        tmp.append(t)
+    arr.append(tmp)
+
+for k in range(len(arr)):
+    # Так как это рукерсия => из 1 элемента будет получаться 3
+    # элемента, что-бы не париться с расширеением массива
+    # можно сделать сразу его двумерным
+    tmp = [arr[k]]
+    function(tmp, len(tmp))
