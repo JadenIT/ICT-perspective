466a02dd68bebed747dd8b4f4d7038cc828fa9d9
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-05-06 12:43:28 +0300

    Updated

---
+++ mccme/1037/1037/1037.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long N;
+
+	cin >> N;
+
+	if (N == 1) {
+		cout << 1 << " " << 1;
+		return 0;
+	}
+
+	for (long long i = N - 1; i >= 1; i--) {
+		if (N % i == 0) {
+			if ((N - i) % i == 0) {
+				cout << i << " " << N - i;
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
+
---
+++ mccme/146/146/146.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	int frst = d;
+	int scnd = b;
+
+	a *= frst;
+	b *= frst;
+
+	c *= scnd;
+	d *= scnd;
+
+	int numerator = a + c;
+	int denominator = b;
+	// Найти НОД t и b
+	int m = min(numerator, denominator);
+	for (int i = m; i > 1; i--) {
+		if (denominator % i == 0 && numerator % i == 0) {
+			int GCD = i;
+			numerator /= GCD;
+			denominator /= GCD;
+			cout << numerator << " " << denominator;
+			return 0;
+		}
+	}
+	cout << numerator << " " << denominator;
+
+
+	return 0;
+}
