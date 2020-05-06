e337ab7afe63f16cd4bf73b9a568e2796944fb7b
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-05-06 16:39:45 +0300

    Updated

---
+++ mccme/113710 (Solved)/113710/113710.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int gcd_euclid(int num_1, int num_2) {
+	while (num_1 != 0 && num_2 != 0) num_1 > num_2 ? num_1 %= num_2 : num_2 %= num_1;
+	return num_1 ? num_1 : num_2;
+}
+
+int main()
+{
+	long long a, b, n, s = 0;
+
+	cin >> a >> b >> n;
+
+	// НОК(a,b) = (a*b) / НОД(a,b)
+
+	long long lcm = a * b / gcd_euclid(a, b);
+
+	long long skippedShops = n / lcm;
+
+	cout << n - skippedShops;
+
+	return 0;
+
+}
