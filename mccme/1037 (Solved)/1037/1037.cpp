0608d4fd64c72c64328fa6ef44f494c385340330
Jaden <55096567+JadenIT@users.noreply.github.com>
2020-05-06 17:29:06 +0300

    Updated

---
+++ mccme/1037 (Solved)/1037/1037.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	long long N;
+
+	cin >> N;
+
+	for (long long i = 2; i * i <= N; i++) {
+		if (N % i == 0) {
+			cout << N / i << " " << N - N / i << endl;
+			return 0;
+		}
+	}
+
+	cout << 1 << " " << N - 1;
+
+	return 0;
+}
+
